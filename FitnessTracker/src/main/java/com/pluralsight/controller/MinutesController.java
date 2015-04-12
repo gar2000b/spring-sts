8150ecafe2c38aad14fb951ff1160b3e7d28ce97
Gary Black <gar2000b@yahoo.com>
2015-04-12 18:57:21 +0100

    adding mvc resources such as pdfs - sets up a controller behind the scenes with a file resolver controller

--- FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
@@ -14,14 +14,15 @@ public class MinutesController {
 		
 		System.out.println("exercise: " + exercise.getMinutes());
 //		return "forward:addMoreMinutes.html"; // forwards the request after it is completed.
-		return "redirect:addMoreMinutes.html"; // re-directs the request.
+//		return "redirect:addMoreMinutes.html"; // re-directs the request.
+		return "addMinutes"; // re-directs the request.
 	}
 
-	@RequestMapping(value = "/addMoreMinutes")
-	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
-		
-		System.out.println("exercising: " + exercise.getMinutes());
-		return "addMinutes";
-	}
+//	@RequestMapping(value = "/addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+//		
+//		System.out.println("exercising: " + exercise.getMinutes());
+//		return "addMinutes";
+//	}
 	
 }
