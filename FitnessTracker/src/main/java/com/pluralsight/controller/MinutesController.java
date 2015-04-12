dafcd37be3795a230ce77c22dff2309e86375deb
Gary Black <gar2000b@yahoo.com>
2015-04-12 18:18:12 +0100

    manipulating view with forwards and re-directs

--- FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
@@ -13,6 +13,14 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		
 		System.out.println("exercise: " + exercise.getMinutes());
+//		return "forward:addMoreMinutes.html"; // forwards the request after it is completed.
+		return "redirect:addMoreMinutes.html"; // re-directs the request.
+	}
+
+	@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		
+		System.out.println("exercising: " + exercise.getMinutes());
 		return "addMinutes";
 	}
 	
